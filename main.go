aa6e3277f97b1cd49d495caea468165bdf981d9c
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-11-21 12:40:40 +0100

    a struct for template

--- main.go
+++ main.go
@@ -6,6 +6,20 @@ import (
 	"log"
 )
 
+//templ represents a single template
+type templateHandler struct {
+	one sync.Once
+	filename string
+	templ *template.Template
+}
+// ServeHTTP handles the HTTP request.
+func (f *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, nil)
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte( "<html><head><title>Lets Chat!</title></head><body><h1>Hi there, Hello World!</h1></body></html>" ))
 }
