bf27f134a317e727ffd9af7b901fdf182d06eea8
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-11-23 19:20:26 +0100

    function run

--- room.go
+++ room.go
@@ -12,4 +12,28 @@ type room struct {
 	clients map[*client]bool
 }
 
-
+func (r *room) run(){
+	for{
+		select {
+		case client := <-r.join:
+			// Joining
+			r.clients[client] = true
+		case client := <-r.leave:
+			// Leaving
+			delete(r.clients, client)
+			close(client.send)
+		case msg := <--r.forward:
+			// forward message to call client
+			for client := range r.clients {
+				select {
+				case client.send <- msg:
+					//send the message
+				default:
+					//failed to send
+					delete(r.clients, client)
+					close(client.send)
+				}
+			}
+		}
+	}
+}
