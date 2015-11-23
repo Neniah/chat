3431627175e12cd3f5ebec9fa163aa5420214291
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-11-23 18:43:28 +0100

    adding to room

--- room.go
+++ room.go
@@ -4,4 +4,12 @@ type room struct {
 	// forward is a channel that holds incoming messages
 	// that should be fowwarded to the other clients.
 	forward chan []byte
+
+	join chan *client
+
+	leave chan *client
+
+	clients map[*client]bool
 }
+
+
