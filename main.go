34518e42e47d1f320406e5cab608f108ad50531e
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-01-05 16:37:27 +0100

    from request.Host

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	var addr = flag.String("addr", ":8080", "The addr of the application.")
-	flag.Parse()
+	flag.Parse() // parse the flags
 
 	r := newRoom()
 	r.tracer = trace.New(os.Stdout)
